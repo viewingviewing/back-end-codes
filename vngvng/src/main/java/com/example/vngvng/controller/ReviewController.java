9ab96f9b82d1f80c2fcfdb621f331aa0b19a12e6
suakang17 <rkdtndk09@gmail.com>
2022-08-07 21:20:35 +0900

    feat:review api post, update

--- vngvng/src/main/java/com/example/vngvng/controller/ReviewController.java
+++ vngvng/src/main/java/com/example/vngvng/controller/ReviewController.java
@@ -1,17 +1,21 @@
 package com.example.vngvng.controller;
 
-import com.example.vngvng.DTO.review.PostReviewDto;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.vngvng.DTO.review.PostReviewRequestDto;
+import com.example.vngvng.DTO.review.UpdateReviewRequestDto;
+import com.example.vngvng.service.ReviewService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/review")
 public class ReviewController {
+    private final ReviewService reviewService;
 
     @PostMapping
-    public Long postReview(@RequestBody PostReviewDto reviewDto){
-        public Long
+    public Long postReview(@RequestBody PostReviewRequestDto requestDto) {
+        return reviewService.postReview(requestDto);
     }
-}
+
+    @PatchMapping("/{id}")
+    public Long updateReview(@PathVariable Long id, @RequestBody UpdateReviewRequestDto requestDto) {
+        return reviewService.updateReview(id, requestDto);
+    }
--- vngvng/src/main/java/com/example/vngvng/service/ReviewService.java
+++ vngvng/src/main/java/com/example/vngvng/service/ReviewService.java
@@ -1,10 +1,31 @@
 package com.example.vngvng.service;
 
+import com.example.vngvng.DTO.review.PostReviewRequestDto;
+import com.example.vngvng.DTO.review.UpdateReviewRequestDto;
+import com.example.vngvng.domain.review.Review;
+import com.example.vngvng.domain.review.ReviewRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class ReviewService {
-    private final ReviewRepository
+    private final ReviewRepository reviewRepository;
+
+    public Long postReview(PostReviewRequestDto requestDto) {
+        return reviewRepository.save(requestDto.toEntity()).getId();
+    }
+
+    public Long updateReview(Long id, UpdateReviewRequestDto requestDto) {
+        Review review = ReviewRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 리뷰가 존재하지 않습니다."));
+        review.reviewUpdate(requestDto.getShow(),
+                requestDto.getVenue(),
+                requestDto.getArtist(),
+                requestDto.getTextReview(),
+                requestDto.getPhotoReviews(),
+                requestDto.getEvaluation(),
+                requestDto.getSeat());
+        return id;
+    }
 }
