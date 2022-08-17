41fb87467b1e7808740ccd586ba7d6b0b10f5b2a
Ji-Hyeon212 <wlgus7464@gmail.com>
2022-08-17 23:23:24 +0900

    validation 코드 작성

---
+++ vngvng/src/main/java/com/example/vngvng/config/BaseResponseStatus.java
@@ -0,0 +1,52 @@
+package com.example.vngvng.config;
+
+import lombok.Getter;
+
+@Getter
+public enum BaseResponseStatus {
+    /**
+     * 1000 : 요청 성공
+     */
+    SUCCESS(true, 1000, "요청에 성공하였습니다."),
+
+
+    /**
+     * 2000 : Request 오류
+     */
+    // Common
+    REQUEST_ERROR(false, 2000, "입력값을 확인해주세요."),
+
+    // [POST] /users
+    POST_USERS_EXISTS_EMAIL(false,2013,"중복된 이메일입니다."),
+    POST_USERS_EXISTS_NICKNAME(false,2014,"중복된 닉네임입니다."),
+
+
+    /**
+     * 3000 : Response 오류
+     */
+    // Common
+    RESPONSE_ERROR(false, 3000, "값을 불러오는데 실패하였습니다."),
+
+
+    /**
+     * 4000 : Database, Server 오류
+     */
+    DATABASE_ERROR(false, 4000, "데이터베이스 연결에 실패하였습니다."),
+    SERVER_ERROR(false, 4001, "서버와의 연결에 실패하였습니다."),
+
+    //[PATCH] /users/{userIdx}
+    MODIFY_FAIL_USERINFO(false,4014,"유저 정보 수정 실패"),
+    //[PATCH] /review/{reviewIdx}
+    MODIFY_FAIL_REVIEWINFO(false,4015,"유저 정보 수정 실패");
+
+
+    private final boolean isSuccess;
+    private final int code;
+    private final String message;
+
+    private BaseResponseStatus(boolean isSuccess, int code, String message) {
+        this.isSuccess = isSuccess;
+        this.code = code;
+        this.message = message;
+    }
+}
